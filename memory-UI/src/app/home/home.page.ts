c9c6f55f9661fa00a6c986993b90eeb2b18c9ac0
Ajla Panjeta <apanjeta2@etf.unsa.ba>
2019-09-10 22:40:02 +0200

    initial commit

---
+++ memory-UI/src/app/home/home.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators
+} from "@angular/forms";
+import { GameService } from "../shared/game.service";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
+})
+export class HomePage implements OnInit {
+  private loginForm: FormGroup;
+ 
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    public _gameService: GameService
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ["", Validators.required]
+    });
+  }
+
+  get f(): any {
+    return this.loginForm.controls;
+  }
+  toStartPage() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this._gameService.setUsername(this.loginForm.get("username").value);
+    this.router.navigate(["/start"]);
+  }
+  
+}
